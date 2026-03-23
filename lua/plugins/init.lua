c58a2168836a2c8913b5c34a9d09f9407342daef
Zach Schneider <me@zachschneider.com>
2026-03-23 10:18:52 -0500

    initial config

---
+++ lua/plugins/init.lua
@@ -0,0 +1,10 @@
+return {
+  require("plugins.theme"),
+  require("plugins.lualine"),
+  require("plugins.neotree"),
+  require("plugins.telescope"),
+  require("plugins.lsp"),
+  require("plugins.cmp"),
+  require("plugins.copilot"),
+  require("plugins.treesitter"),
+}
